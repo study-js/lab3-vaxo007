82958d7c39305ec247fbb4c9d48761823343a5f2
vaxo007 <vakhobitadze@gmail.com>
2017-11-18 12:33:27 +0400

    l305

---
+++ L305/script.js
@@ -0,0 +1,12 @@
+var sheyvana = +prompt("Sheiyvane ");
+var students = [];
+
+for (var i =0; i < sheyvana; ++i){
+    var saxelebi = prompt("saxeli");
+    students.push(saxelebi);
+
+    
+}
+
+students.sort();
+document.write(students);
